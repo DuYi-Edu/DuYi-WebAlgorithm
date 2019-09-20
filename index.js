0010cf0e3b4d70c52c9caa3e5620b75eaac04748
clflovepanda <982294498@qq.com>
2019-09-20 13:57:53 +0800

    init

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-var b = {
-    value: 2,
-    next: null
+function Node(value) {
+    this.value = value;
+    this.next = null;
 }
 
-var a = {
-    value: 1,
-    next: b
-}
+var a = new Node(1);
+var b = new Node(2);
+var c = new Node(3);
+var d = new Node(4);
 
-console.log(a.next == b);
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = null;
+
+console.log(a.value);
+console.log(a.next.value);
+console.log(a.next.next.value);
+console.log(a.next.next.next.value);
