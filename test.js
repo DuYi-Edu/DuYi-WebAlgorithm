c4c663792974619c63ea447ffc6666b8bdddccb8
clflovepanda <982294498@qq.com>
2019-09-25 20:33:13 +0800

    init

--- test.js
+++ test.js
@@ -20,26 +20,43 @@ b1.left = d1;
 b1.right = e1;
 
 var a2 = new Node("a");
-var b2 = new Node("b");
+var b2 = new Node("z");
 var c2 = new Node("c");
-var d2 = new Node("d");
+var d2 = new Node("x");
 var e2 = new Node("e");
 var f2 = new Node("f");
 var g2 = new Node("g");
 
-a2.right = c2;
-a2.left = b2;
+a2.left = c2;
+a2.right = b2;
 c2.left = f2;
-c2.right = g2;
+// c2.right = g2;
 b2.left = d2;
 b2.right = e2;
+f2.right = g2;
 
-function compareTree(root1, root2) {
-    if (root1 == root2) return true;//是同一个颗树
-    if (root1 == null && root2 != null || root2 == null && root1 != null) return false;//其中一个为空，另一个不为空
-    if (root1.value != root2.value) return false;//相同位置的值不相等
-    return compareTree(root1.left, root2.left) && compareTree(root1.right, root2.right)
-    || compareTree(root1.left, root2.right) && compareTree(root1.right, root2.left);
+//新增了什么，修改了什么，删除了什么
+
+// {type: "新增", origin: null, now: c2},
+// {type: "修改", origin: c1, now: c2},
+// {type: "删除", origin: c2, now: null }
+// var diffList = [];
+
+function diffTree(root1, root2, diffList) {
+    if (root1 == root2) return diffList;
+    if (root1 == null && root2 != null) {// 新增了节点
+        diffList.push({type: "新增", origin: null, now: root2});
+    } else if (root1 != null && root2 == null) {// 删除了节点
+        diffList.push({type: "删除", origin: root1, now: null});
+    } else if (root1.value != root2.value) {//相同位置的节点值不同了，修改了节点
+        diffList.push({type: "修改", origin: root1, now: root2});
+        diffTree(root1.left, root2.left, diffList);
+        diffTree(root1.right, root2.right, diffList);
+    } else {
+        diffTree(root1.left, root2.left, diffList);
+        diffTree(root1.right, root2.right, diffList);
+    }
 }
-
-console.log(compareTree(a1, a2));
+var diffList = [];
+diffTree(a1, a2, diffList);
+console.log(diffList);
