66e316566c40ee7961343bb4318c6d807574b5a5
clflovepanda <982294498@qq.com>
2019-09-26 04:06:00 +0800

    init

--- test.js
+++ test.js
@@ -4,14 +4,16 @@ function Node(value) {
     this.right = null;
 }
 
-var node2 = new Node("2");
-var node5 = new Node("5");
-var node3 = new Node("3");
+var node8 = new Node("8");
+var node7 = new Node("7");
 var node6 = new Node("6");
+var node5 = new Node("5");
+var node2 = new Node("2");
 
-node2.right = node5;
-node5.left = node3;
-node5.right = node6;
+node8.left = node7;
+node7.left = node6;
+node6.left = node5;
+node5.left = node2;
 
 function getDeep(root) {
     if (root == null) return 0;
@@ -66,15 +68,29 @@ function change(root) {//返回平衡之后的根节点
     if (Math.abs(leftDeep - rightDeep) < 2) {
         return true;
     } else if (leftDeep > rightDeep) {//不平衡，左边深，需要右旋
+        var changeTreeDeep = getDeep(root.left.right);
+        var noChangeTreeDeep = getDeep(root.left.left);
+        if (changeTreeDeep > noChangeTreeDeep) {
+            root.left = leftRotate(root.left);
+        }
         return rightRotate(root);
     } else {//不平衡，右边深，需要左旋
+        var changeTreeDeep = getDeep(root.right.left);
+        var noChangeTreeDeep = getDeep(root.right.right);
+        if (changeTreeDeep > noChangeTreeDeep) {
+            root.right = rightRotate(root.right);
+        }
         return leftRotate(root);
     }
 }
 
-console.log(isBalance(node2));
+// console.log(isBalance(node8));
+//
+// var newRoot = change(node8);
+//
+// console.log(isBalance(newRoot));
+// console.log(newRoot);
 
-var newRoot = change(node2);
+for () {
 
-console.log(isBalance(newRoot));
-console.log(newRoot);
+}
