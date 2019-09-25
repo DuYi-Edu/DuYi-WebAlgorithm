cdefe1589857b1df576550ca9c622e933bb9d546
clflovepanda <982294498@qq.com>
2019-09-26 04:00:13 +0800

    init

--- test.js
+++ test.js
@@ -4,24 +4,14 @@ function Node(value) {
     this.right = null;
 }
 
-var a = new Node("a");
-var b = new Node("b");
-var c = new Node("c");
-var d = new Node("d");
-var e = new Node("e");
-var f = new Node("f");
-var g = new Node("g");
-var h = new Node("h");
-var j = new Node("j");
+var node2 = new Node("2");
+var node5 = new Node("5");
+var node3 = new Node("3");
+var node6 = new Node("6");
 
-a.left = b;
-a.right = c;
-b.left = d;
-// b.right = e;
-c.left = f;
-c.right = g;
-d.right = h;
-// e.right = j;
+node2.right = node5;
+node5.left = node3;
+node5.right = node6;
 
 function getDeep(root) {
     if (root == null) return 0;
@@ -41,4 +31,50 @@ function isBalance(root) {
     }
 }
 
-console.log(isBalance(b));
+function leftRotate(root) {
+    // 找到新根
+    var newRoot = root.right;
+    // 找到变化分支
+    var changeTree = root.right.left;
+    // 当前旋转节点的右孩子为变化分支
+    root.right = changeTree;
+    // 新根的左孩子为旋转节点
+    newRoot.left = root;
+    // 返回新的根节点
+    return newRoot;
+}
+
+function rightRotate(root) {
+    // 找到新根
+    var newRoot = root.left;
+    // 找到变化分支
+    var changeTree = root.left.right;
+    // 当前旋转节点的左孩子为变化分支
+    root.left = changeTree;
+    // 新根的右孩子为旋转节点
+    newRoot.right = root;
+    // 返回新的根节点
+    return newRoot;
+}
+
+function change(root) {//返回平衡之后的根节点
+    if (isBalance(root)) return root;
+    if (root.left != null) root.left = change(root.left);
+    if (root.right != null) root.right = change(root.right);
+    var leftDeep = getDeep(root.left);
+    var rightDeep = getDeep(root.right);
+    if (Math.abs(leftDeep - rightDeep) < 2) {
+        return true;
+    } else if (leftDeep > rightDeep) {//不平衡，左边深，需要右旋
+        return rightRotate(root);
+    } else {//不平衡，右边深，需要左旋
+        return leftRotate(root);
+    }
+}
+
+console.log(isBalance(node2));
+
+var newRoot = change(node2);
+
+console.log(isBalance(newRoot));
+console.log(newRoot);
